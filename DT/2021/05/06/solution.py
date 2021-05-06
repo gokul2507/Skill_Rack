454d7e4cbe498e672283eabde1d8f36767eeed9f
RexMilton <rexmilton5@gmail.com>
2021-05-06 22:26:19 +0530

    m

--- DT/2021/05/06/solution.py
+++ DT/2021/05/06/solution.py
@@ -4,7 +4,7 @@ z=[input().strip() for _ in range(a-1)]
 l=len(t[0])
 for i in z:
     while i:
-        t.append(i[:min(len(i),l)]+(""(l-min(len(i),l))))
+        t.append(i[:min(len(i),l)]+("*"*(l-min(len(i),l))))
         i=i[min(len(i),l):]
 for i in list(zip(*t)):
     print(*i)
