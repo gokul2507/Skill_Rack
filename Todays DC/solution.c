456e4170c70f96485f6c82611711059f71d868cd
RexMilton <rexmilton5@gmail.com>
2021-05-09 21:34:35 +0530

    m

--- Todays DC/solution.c
+++ Todays DC/solution.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 char* getCommaSeparatedValues(int SIZE, int arr[])
 {
     char *answer=malloc(sizeof(char [101]));
@@ -12,4 +14,15 @@ char* getCommaSeparatedValues(int SIZE, int arr[])
         *(answer+t++)=',';
     }
     return answer;
+}
+int main(){ 
+    int N; 
+    scanf("%d", &N); 
+    int arr[N]; 
+    for(int index = 0; index < N; index++) { 
+        scanf("%d", &arr[index]); 
+        } 
+    char *ptr = getCommaSeparatedValues(N, arr); 
+    printf("CSV: %s", ptr); 
+    return 0; 
 }
