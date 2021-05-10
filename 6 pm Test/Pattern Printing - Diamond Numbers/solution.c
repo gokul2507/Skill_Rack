6525417606faf75609fb8d98516d6eebe94dd28b
RexMilton <rexmilton5@gmail.com>
2021-05-10 19:26:07 +0530

    m

--- 6 pm Test/Pattern Printing - Diamond Numbers/solution.c
+++ 6 pm Test/Pattern Printing - Diamond Numbers/solution.c
@@ -1,9 +1,31 @@
 #include <stdio.h>
-void printPattern(int n){
-    int x=1;
-    for(int i=1;i<n*2;i++){
-        
+void zero(int n){
+    while(n--)
+    printf("0 ");
+}
+void num(int n){
+    printf("%d ",n);
+}
+void printPattern(int N)
+{
+    int l=1,r=(N-1)*4,x=-1;
+    for(int i=1;i<N+N;i++){
+        zero(abs(N-i));
+        if(i==1 ||i==N+N-1)
+        num(l++);
+        else{
+            num(l++);
+            zero(x);
+            num(r--);
+        }
+        if(N-i>0)
+        x+=2;
+        else
+        x-=2;
+        zero(abs(N-i));
+        printf("\n");
     }
+    
 }
 int main()
 {
@@ -12,9 +34,3 @@ int main()
     printPattern(N);
     return 0;
 }
-
-/*0 0 1 0 0
-  0 2 0 8 0
-  3 0 0 0 7
-  0 4 0 5 0 
-  0 0 6 0 0*/
