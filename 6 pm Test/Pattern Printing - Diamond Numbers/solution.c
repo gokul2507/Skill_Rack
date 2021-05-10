6186523d106c7e5f9813f5fb5c09a4526ab1c391
RexMilton <rexmilton5@gmail.com>
2021-05-10 19:23:35 +0530

    m

---
+++ 6 pm Test/Pattern Printing - Diamond Numbers/solution.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void printPattern(int n){
+    int x=1;
+    for(int i=1;i<n*2;i++){
+        
+    }
+}
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    printPattern(N);
+    return 0;
+}
+
+/*0 0 1 0 0
+  0 2 0 8 0
+  3 0 0 0 7
+  0 4 0 5 0 
+  0 0 6 0 0*/
---
+++ 6 pm Test/Pattern Printing - Start Number/solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int start = sc.nextInt();
+        int N = sc.nextInt();
+        printPattern(start, N);
+        
+    }
+}
