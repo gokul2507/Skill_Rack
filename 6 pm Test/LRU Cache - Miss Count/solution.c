2cd6d3c8e7ffc5cf09d9d73602478c79b6671362
RexMilton <rexmilton5@gmail.com>
2021-05-11 20:12:32 +0530

    m

--- 6 pm Test/LRU Cache - Miss Count/solution.c
+++ 6 pm Test/LRU Cache - Miss Count/solution.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
-
+int lruCountMiss(int n, int *l, int len)
+{
+    int s[n];
+    int c=0;
+    for(int i=0;i<n;i++) s[i]=-1;
+    for(int i=0;i<len;i++){
+        for(int j=0;j<n;j++){
+            if(l[i]==s[j]){
+                for(int k=j;k<n-1;k++) s[k]=s[k+1];
+                s[n-1]=l[i];
+                break;
+            }
+            else if(j==(n-1)){
+                for(int m=0;m<n;m++) s[m]=s[m+1];
+                s[n-1]=l[i];
+                c++;
+            }
+        }
+    }
+    return c;
+}
 void main()
 {
     int N, MAX_CACHE_SIZE;
