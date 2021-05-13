31c87a797cc95624c55b9526217973ccb16e4e1c
RexMilton <rexmilton5@gmail.com>
2021-05-13 20:17:06 +0530

    m

--- 6 pm Test/Pattern Printing - Diamond Numbers/solution.py
+++ 6 pm Test/Pattern Printing - Diamond Numbers/solution.py
@@ -1,29 +1,17 @@
+inc=1
 a=int(input())
-l=[[0 for _ in range(a+a-1)] for _ in range(a+a-1)]
-x,y=0,a-1
-t=1
-while y>=0:
-    l[x][y]=t
-    t+=1
-    x+=1
-    y-=1
-y+=2
-while x<a+a-1:
-    l[x][y]=t
-    t+=1
-    x+=1
-    y+=1
-x-=2
-while y<a+a-1:
-    l[x][y]=t
-    t+=1
-    y+=1
-    x-=1
-y-=2
-while x:
-    l[x][y]=t
-    t+=1
-    y-=1
-    x-=1
-for i in l:
-    print(*i)
+l,r=1,(a-1)*4
+x,y=a-1,a-1
+for i in range(a+a-1):
+    z=["0" for _ in range(a+a-1)]
+    if x==y:
+        z[x]=l
+    else:
+        z[x],z[y]=l,r
+        r-=1
+    l+=1
+    x-=inc
+    y+=inc
+    if x==0:
+        inc=-1
+    print(z)
