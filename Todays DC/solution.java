a67557f6bc3f495f95a7d57f21f9916d72c466ec
RexMilton <rexmilton5@gmail.com>
2021-05-15 18:13:45 +0530

    m

--- Todays DC/solution.java
+++ Todays DC/solution.java
@@ -1,7 +1,24 @@
 import java.text.DecimalFormat;
 import java.util.*;
+class Person{
+    private String s;
+    private int w;
+    private double h;
+    Person(String s,int w,double h){
+        this.s=s;
+        this.w=w;
+        this.h=h;
+    }
+    public double getBodyMassIndex(){
+        return w/(h*h);
+    }
+    @Override
+    public String toString(){
+        String H=String.format("%.2f",h);
+    return "Name: "+s+"\n"+"Weight: "+w+" kg\n"+"Height: "+H+" m";
+    }
+}
 public class Hello {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String name = sc.nextLine().trim();
