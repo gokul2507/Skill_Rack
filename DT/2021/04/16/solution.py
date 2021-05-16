72380d8ac8cae46e44afea946eb2b21fd79ff162
gokul2507 <66557183+gokul2507@users.noreply.github.com>
2021-05-16 12:00:47 +0530

    Delete DT/2021 directory

--- DT/2021/04/16/solution.py
+++
@@ -1,5 +0,0 @@
-a='abcdefghijklmnopqrstuvwxyz'
-for i in range(int(input())):
-    x=input().strip()
-    y=x.lower().index(a[i%26])
-    print(x[y:])
--- DT/2021/05/03/solution.py
+++
@@ -1,5 +0,0 @@
-a=input().strip()
-x=int(input())
-for i in range(1,len(a)+x):
-    s="*"*max(0,i-x)+a[max(0,i-x):i]+"*"*max(0,len(a)-i)
-    print(s+s[-2::-1])
