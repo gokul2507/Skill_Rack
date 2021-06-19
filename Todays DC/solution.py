3cbdc5ee4d922ad2ae04e7cfd4fe0210872581b2
RexMilton <rexmilton5@gmail.com>
2021-06-19 23:17:16 +0530

    m

--- Todays DC/solution.py
+++ Todays DC/solution.py
@@ -1,3 +1,13 @@
+class MaximumLengthExceededException(Exception):
+    def __init__(self, S):
+        self.S=S
+    def __str__(self):
+        return self.S
+class NegativeValueException(Exception):
+    def __init__(self, s):
+        self.s=s 
+    def __str__(self):
+        return self.s
 name = input().strip()
 age = int(input().strip())
 try:
