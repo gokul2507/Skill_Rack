8e7a63d15d3357289739e6e1336705354623f3a8
RexMilton <rexmilton5@gmail.com>
2021-07-02 22:06:57 +0530

    m

--- DT/2021/07/01/solution.c
+++ DT/2021/07/01/solution.c
@@ -7,10 +7,26 @@ typedef struct BoundedArray
     int SIZE;
     char **words;
 } boundedArray;
- 
+_Bool isvow(char c){
+    c=tolower(c);
+    return c=='a' || c=='e' || c=='o' || c=='u' || c=='i';
+}
 boundedArray* getWordsStartingWithVowel(char str[])
 {
-    
+    boundedArray *ptr=malloc(sizeof(boundedArray));
+    ptr->words=(char**)malloc(1000*sizeof(char*));
+    for(int i=0; i<1000; i++)
+    ptr->words[i]=(char*)malloc(1000);
+    ptr->SIZE=0;
+    char *s=strtok(str," ");
+    while(s){
+        if(isvow(s[0]))
+        strcpy(ptr->words[ptr->SIZE++],s);
+        s=strtok(0," ");
+    }
+    if(!ptr->SIZE)
+    ptr->words[ptr->SIZE++]="-1";
+    return ptr;
 }
 int main()
 {
@@ -25,4 +41,3 @@ int main()
     }
     return 0;
 }
-
