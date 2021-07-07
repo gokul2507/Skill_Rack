35d486529cd0a03aff97bf0fa1bbd1b5ed92b25d
RexMilton <rexmilton5@gmail.com>
2021-07-07 22:06:36 +0530

    m

--- DT/2021/07/06/solution.c
+++ DT/2021/07/06/solution.c
@@ -7,10 +7,29 @@ typedef struct BoundedArray
     int SIZE;
     char **words;
 } boundedArray;
- 
 boundedArray* concatEveryTwoWords(char str[])
 {
-    
+    boundedArray *bArr=malloc(sizeof(boundedArray));
+    bArr->words=malloc(sizeof(int*)*1001);
+    for(int index=0;index<1001;index++){
+        bArr->words[index]=malloc(sizeof(char)*1001);
+    }
+    bArr->SIZE=0;
+    char *word=strtok(str," ");
+    int flag=1;
+    while(word!=NULL){
+        if(flag){
+            strcpy(bArr->words[bArr->SIZE],word);
+            flag^=1;
+        }
+        else
+        {
+            strcat(bArr->words[bArr->SIZE++],word);
+            flag^=1;
+        }
+        word=strtok(NULL," ");
+    }
+    return bArr;
 }
 int main()
 {
