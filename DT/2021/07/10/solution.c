e7ac8c3cb6112d28e82bc158a3756358cfc71d1f
RexMilton <rexmilton5@gmail.com>
2021-07-10 22:20:43 +0530

    m

---
+++ DT/2021/07/10/solution.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+void readMatrixFromFile(char fileName[])
+{
+    
+}
+int main()
+{
+    char fileName[101];
+    scanf("%s", fileName);
+    readMatrixFromFile(fileName);
+    return 0;
+}
