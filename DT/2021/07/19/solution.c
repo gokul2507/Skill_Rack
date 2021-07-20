a642e79abca9a49fb17b16e1b1606f13415baaa0
RexMilton <rexmilton5@gmail.com>
2021-07-20 18:34:00 +0530

    m

--- DT/2021/07/19/solution.c
+++ DT/2021/07/19/solution.c
@@ -1,10 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+#include<string.h>
 void transposeMatrix(char *fileName)
 {
-    
-} // end of transposeMatrix function
+    FILE *fp = fopen(fileName,"r");
+    char temp[1001];
+    int mat[1001][1001];
+    int k=0,l=0;
+    while(fgets(temp,1001,fp)!=NULL)
+    {
+        char *tok = strtok(temp," ");
+        l=0;
+        while(tok!=NULL)
+        {
+            int val = atoi(tok);
+            mat[k][l++] = val;
+            tok = strtok(NULL," ");
+        }
+        k++;
+    }
+    fp = fopen(fileName,"w");
+    for(int i=0;i<l;i++)
+    {
+        for(int j=0;j<k;j++)
+        {
+            fprintf(fp,"%d ",mat[j][i]);
+        }
+        fprintf(fp,"%c",'\n');
+    }
+    fclose(fp);
+}
 void printFileContent(char *fileName)
 {
     FILE *fp = fopen(fileName, "r");
