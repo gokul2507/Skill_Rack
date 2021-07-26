8af608b2a2c1d7ae12e0a360e91db0c7ad3a92ac
RexMilton <rexmilton5@gmail.com>
2021-07-26 22:07:19 +0530

    m

---
+++ DT/2021/07/26/solution.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+void swapFileContent(char *file1name, char *file2name)
+{
+    
+} // end of swapFileContent function
+void printFileContent(char *filename)
+{
+    FILE *fp = fopen(filename,"r");
+    char ch;
+    while((ch = fgetc(fp)) != EOF)
+    {
+        printf("%c", ch);
+    }
+    fclose(fp);
+} // end of printFileContent function
+
+int main()
+{
+    char file1name[31], file2name[31];
+    scanf("%s\n%s", file1name, file2name);
+    swapFileContent(file1name, file2name);
+    printf("File %s:\n", file1name);
+    printFileContent(file1name);
+    printf("\nFile %s:\n", file2name);
+    printFileContent(file2name);
+    return 0;
+} // end of main function
+
