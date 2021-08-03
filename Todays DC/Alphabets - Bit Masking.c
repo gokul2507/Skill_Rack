cc56f619bbbebb8910b692835d1ee38e167da157
RexMilton <rexmilton5@gmail.com>
2021-08-03 20:49:05 +0530

    m

---
+++ Todays DC/Alphabets - Bit Masking.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char str[101];
+    scanf("%s", str);
+    int X = 0;
+    for(int index = 0; str[index] != '\0'; index++)
+    {
+        X =  ;
+    }
+    printf("%d", X);
+    return 0;
+}
