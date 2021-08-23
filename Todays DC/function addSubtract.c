f63efacde7527590d26fac334fba22f12fbcb6b4
RexMilton <rexmilton5@gmail.com>
2021-08-23 22:45:35 +0530

    m

--- Todays DC/function addSubtract.c
+++ Todays DC/function addSubtract.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
-int addSubtract(char *str)
+#include <string.h>
+int addSubtract(char *s)
 {
+    int ctr=0,sum=0;
+    s=s+1;
+    s[strlen(s)-1]='\0';
+    char *t=strtok(s,")(");
+    while(t!=NULL){
+        int temp;
+        sscanf(t,"%d",&temp);
+        if(ctr==0) sum=temp;
+        else{
+            if(ctr%2) sum+=temp;
+            else sum-=temp;
+        }
+        t=strtok(NULL,")(");
+        ctr++;
+    }
+    return sum;
 }
 
 int main()
